b4c7b7ab59940bad09b927d554b748bd9972ffa4
Cem Gurhan <cemg@live.co.uk>
2022-08-27 21:14:32 +0100

    added access abstraction to access storage using a currencyschema, defined transactions in proto, created a service interface for transaction logic and defined generic errors for transactions

--- src/lib.rs
+++ src/lib.rs
@@ -58,4 +58,59 @@ impl Wallet {
     }
 }
 
+#[derive(Debug, FromAccess)]
+pub struct CurrencySchema<T: Access> {
+    /// Correspondence of public keys of users to the account information.
+    pub wallets: MapIndex<T::Base, PublicKey, Wallet>,
+}
+
+impl<T: Access> CurrencySchema<T> {
+    pub fn new(access: T) -> Self {
+        Self::from_root(access).unwrap()
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, ProtobufConvert, BinaryValue)]
+#[protobuf_convert(source = "proto::TxCreateWallet")]
+pub struct TxCreateWallet {
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, ProtobufConvert, BinaryValue)]
+#[protobuf_convert(source = "proto::TxTransfer")]
+pub struct TxTransfer {
+    pub to: PublicKey,
+    pub amount: u64,
+    pub seed: u64,
+}
+
+/// Cryptocurrency service transactions.
+#[exonum_interface]
+pub trait CryptocurrencyInterface<Ctx> {
+    /// Output of the methods in this interface.
+    type Output;
+
+    /// Creates wallet with the given `name`.
+    #[interface_method(id = 0)]
+    fn create_wallet(&self, ctx: Ctx, arg: TxCreateWallet) -> Self::Output;
+    /// Transfers `amount` of the currency from one wallet to another.
+    #[interface_method(id = 1)]
+    fn transfer(&self, ctx: Ctx, arg: TxTransfer) -> Self::Output;
+}
+
+/// Error codes emitted by `TxCreateWallet` and/or `TxTransfer`
+/// transactions during execution.
+#[derive(Debug, ExecutionFail)]
+pub enum Error {
+    /// Wallet already exists.
+    WalletAlreadyExists = 0,
+    /// Sender doesn't exist.
+    SenderNotFound = 1,
+    /// Receiver doesn't exist.
+    ReceiverNotFound = 2,
+    /// Insufficient currency amount.
+    InsufficientCurrencyAmount = 3,
+    /// Sender same as receiver.
+    SenderSameAsReceiver = 4,
+}
 
